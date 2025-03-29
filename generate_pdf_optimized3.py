a5e4bf25017b3519d4890b6157be42e65728ab35
pierrelafo <pierrelaf@gmail.com>
2025-03-29 15:40:41 +0100

    Update generate_pdf_optimized3.py

--- generate_pdf_optimized3.py
+++ generate_pdf_optimized3.py
@@ -53,7 +53,7 @@ def generate_pdf_with_title_and_comments(input_excel_path, first_page_text, imag
 	#	#	#	#	#	#	#	#   Debut Titre	#	#	#	#	#	#	#	#	#	#
 	# Ajouter le titre
 	c.setFillColorRGB(*custom_color2)
-	c.setFont("Arial", 20)
+	#c.setFont("Arial", 20)
 	c.drawCentredString(page_width/2, 680, f"360° de {prenom} {nom}")
 	# Initialiser les styles pour le titre de la section PDF
 	styles = getSampleStyleSheet()
